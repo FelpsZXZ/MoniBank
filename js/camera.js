062442d71a1ca437795730b4ebf18909bfc78f98
FelpsZXZ <157402795+FelpsZXZ@users.noreply.github.com>
2024-04-27 13:20:13 -0300

    Add files via upload

--- js/camera.js
+++ js/camera.js
@@ -1,37 +1,39 @@
-const botaoInicarCamera = document.querySelector("[data-video-botao]"); 
+const botaoIniciarCamera = document.querySelector("[data-video-botao]");
 const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
 const botaoTirarFoto = document.querySelector("[data-tirar-foto]");
 const canvas = document.querySelector("[data-video-canvas]");
 const mensagem = document.querySelector("[data-mensagem]");
-const botaoEnviarFoto = document.querySelector("[data-enviar]");
+const botaoEnviarFoto = document.querySelector("[data-enviar]")
 
-botaoInicarCamera.addEventListener("click", async function () {
-    const inciarVideo = await navigator.mediaDevices.getUserMedia({video: true, audio: false});
+let imagemURL = '';
 
-    botaoInicarCamera.style.display = "none";
+botaoIniciarCamera.addEventListener('click', async function () {
+    const iniciarVideo = await navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false });
+
+    botaoIniciarCamera.style.display = "none";
     campoCamera.style.display = "block";
-    video.srcObject = inciarVideo;
 
-})
+    video.srcObject = iniciarVideo;
+});
 
-botaoTirarFoto.addEventListener("click", function () {
+botaoTirarFoto.addEventListener('click', function () {
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-    const imagemBase64 = canvas.toDataURL("image/jpeg");
+
+    imagemURL = canvas.toDataURL('image/jpeg');
+
     campoCamera.style.display = "none";
     mensagem.style.display = "block";
-    botaoInicarCamera.style.display = "block";
-    video.srcObject = null;
-    console.log(imagemBase64)
-    })
+});
 
-botaoEnviarFoto.addEventListener("click", () => {
+botaoEnviarFoto.addEventListener('click', () => {
     const receberDadosExistentes = localStorage.getItem("cadastro");
-    const converterRetorno = JSON.parse(receberDadosExistentes);
+    const converteRetorno = JSON.parse(receberDadosExistentes);
 
-    converterRetorno.imagem = imagemBase64;
+    converteRetorno.imagem = imagemURL;
 
-    localStorage.setItem("cadastro", JSON.stringify(converterRetorno));
+    localStorage.setItem('cadastro', JSON.stringify(converteRetorno))
 
-    window.location.href = "../pages/abrir-conta-form-3.html";
+    window.location.href = '../pages/abrir-conta-form-3.html';
 })
